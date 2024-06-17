148848c7bb3824fccdbec0cfc527216fb5f7e407
fikrat99 <128935402+fikrat99@users.noreply.github.com>
2024-06-17 10:10:44 -0400

    Transfer to github

---
+++ src/constants/index.js
@@ -0,0 +1,215 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    
+    
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    
+   
+    
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+
+    {
+        imageUrl: express,
+        name: "bootstrap",
+        type: "Frontend",
+    },
+
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+
+    
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Technology Operations",
+        company_name: "Minaret Real States",
+        icon: starbucks,
+        iconBg: "#FFE761",
+        date: "June 2021 - October 2021",
+        points: [
+            "Assisted in network maintenance and troubleshooting, enhancing system reliability, and provided technical support for resolution of complex hardware and software malfunctions",
+            "Collaborated on cybersecurity implementations and system upgrades, contributing to robust data protection and technological advancements using cutting-edge tools.",
+            "Managed IT asset lifecycle and conducted research on emerging tech trends, supporting efficient inventory control and driving strategic recommendations for IT infrastructure enhancements.",
+        ],
+    },
+    
+    {
+        title: "Web Developer",
+        company_name: "Fikrat",
+        icon: shopify,
+        iconBg: "#2bb673",
+        date: "May 2022 - Oct 2023",
+        points: [
+           "Designed an interactive tourism consultancy website with secure online payment processing, enabling clients to  conveniently book and pay for services online.",
+            "Enhanced website visibility through effective SEO techniques, ensuring Mount Kayon ranks prominently in web searches and attracts more visitors..",
+            "Provided a full suite of web services including custom design, hosting, domain registration, and SSL security, alongside advanced features like mobile compatibility, responsive design, and user profile management to ensure a seamless user experience.",
+         ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "Asnadu ",
+        icon: meta,
+        iconBg: "#ffecd4",
+        date: "June 2024 - Present",
+        points: [
+            "Developed and maintained a robust community platform enabling members to connect, share resources, and support each other.",
+            "Implemented advanced features including real-time chat, event scheduling, and resource tracking, enhancing the overall functionality and user experience of the platform.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/imranFshamil',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/imranshamil',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: threads,
+        theme: 'btn-back-red',
+        name: 'AI-Powered 3D product website',
+        description: 'Horizon is a modern banking platform for everyone. ',
+        link: 'https://github.com/imranFshamil/AI-Powered_3D_product_website',
+    },
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-green',
+        name: 'Banking-App-with-Finance-Management',
+        description: ' A modern banking platform for everyone.  ',
+        link: 'https://github.com/imranFshamil/Banking-App-with-Finance-Management',
+    },
+        
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Full Stack Instagram Clone',
+        description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
+        link: 'https://github.com/imranFshamil/Instagram_Clone',
+    },
+    
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'AI Summarizer Application',
+        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
+        link: 'https://github.com/imranFshamil/AI_Summarizer_Applicationr',
+    }
+];
